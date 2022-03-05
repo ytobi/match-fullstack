d2fa0666fc5f872621855a013d49bdac9311e132
Tobi Obadiah <tobiobadiah67@gmail.com>
2022-03-05 10:12:44 +0100

    fix: return correct reponse

--- backend/user/apis.py
+++ backend/user/apis.py
@@ -71,7 +71,7 @@ class UserCreate(generics.CreateAPIView):
         if serializer.is_valid():
             saved_user = serializer.save()
             serializer = UserSerializer(saved_user)
-            return Response(data=serializer.data, status=status.HTTP_200_OK )
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED )
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
